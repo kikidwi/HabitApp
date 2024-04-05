7f92f8c88491e51ecff2f3288c5a05d2ff9f827c
kiki <kikidprasetyo29@gmail.com>
2024-04-05 19:58:57 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -0,0 +1,48 @@
+package com.dicoding.habitapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.dicoding.habitapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            //TODO 11 : Update theme based on value in ListPreference
+            val ListPreference = preferenceManager.findPreference<ListPreference>(getString(R.string.pref_key_dark))
+            ListPreference?.onPreferenceChangeListener =
+                Preference.OnPreferenceChangeListener { _, newValue ->
+                    when (newValue) {
+                        getString(R.string.pref_dark_follow_system) -> updateTheme(-1)
+                        getString(R.string.pref_dark_on) -> updateTheme(2)
+                        getString(R.string.pref_dark_off) -> updateTheme(1)
+                    }
+                    true
+                }
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
+        }
+    }
+}
